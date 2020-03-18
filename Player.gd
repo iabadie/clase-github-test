8d5dafa08866be567b85874593c76deedab3f1fd
Nacho <ignacioabadie@live.com.ar>
2020-03-18 19:14:26 -0300

    movimiento personaje

--- Player.gd
+++ Player.gd
@@ -12,12 +12,16 @@ func _physics_process(delta):
 	var selectedAnimation;
 	if Input.is_pressed('up'):
 		selectedAnimation = "upAnimation";
+		velocity.y += 10
 	elif Input.is_pressed('down'):
 		selectedAnimation = "downAnimation";
+		velocity.y -= 10
 	elif Input.is_pressed('left'):
 		selectedAnimation = "leftAnimation";
-	elsif Input.is_pressed('right'):
+		velocity.x -= 10
+	elif Input.is_pressed('right'):
 		selectedAnimation = "rightAnimation";
+		velocity.x += 10
 	else:
 		selectedAnimation = null;
 	
@@ -31,14 +35,12 @@ func _physics_process(delta):
 	move_and_slide(velocity)
 	pass
 
-	# Detecta si se pareta el boton y ejecuta un golpe.
-	func hit_detection():
-		if Input.is_pressed('space_bar'):
+# Detecta si se pareta el boton y ejecuta un golpe.
+func hit_detection():
+	if Input.is_pressed('space_bar'):
 
-		pass
+	pass
 
-	func hit():
-		hit_action()
-		pass
-
-
+func hit():
+	hit_action()
+	pass
